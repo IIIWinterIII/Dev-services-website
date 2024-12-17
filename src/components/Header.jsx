76090fcb3fc7f737c4d1984291df69358126b6e3
IIIWinterIII <nikitavakulin39@gmail.com>
2024-12-17 16:03:09 +0300

    adding icons to the header and adjusting styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import routes from "../routes";
-import "../styles/components/Header.css"
+import "../styles/components/Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
@@ -11,13 +13,21 @@ function Header() {
         <ul>
           {routes.map(({ path, name, component: Component }, index) => (
             <li key={index}>
-              <Link to={path} component={<Component />} className="link" >
+              <Link to={path} component={<Component />} className="link">
                 {name}
               </Link>
             </li>
           ))}
         </ul>
       </nav>
+      <div className="contacts-button">
+        <a>
+          <FontAwesomeIcon className="icon" icon={faPaperPlane} />
+        </a>
+        <a>
+          <FontAwesomeIcon className="icon" icon={faEnvelope} />
+        </a>
+      </div>
     </header>
   );
 }
