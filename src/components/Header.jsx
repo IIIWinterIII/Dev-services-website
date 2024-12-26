41c84b9f2f5ad8d8e25ae18e71cfd7a71ca9ffc1
IIIWinterIII <nikitavakulin39@gmail.com>
2024-12-26 18:23:21 +0300

    creating a header structure and styles for it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import "../styles/components/Header.scss";
 
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import routes from "../routes";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaperPlane, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faWhatsapp, faTelegram } from "@fortawesome/free-brands-svg-icons";
 
 function Header() {
+  const { pathname } = useLocation();
+  
   return (
     <header>
       <h1>Logo</h1>
@@ -15,19 +18,23 @@ function Header() {
         <ul>
           {routes.map(({ path, name, component: Component }, index) => (
             <li key={index}>
-              <Link to={path} component={<Component />} className="link">
-                {name}
+              <Link to={path} component={<Component />} className={`link ${path === pathname ? "active" : ''}`}>
+                <span>{name}</span>
               </Link>
             </li>
           ))}
         </ul>
       </nav>
       <div className="contacts-button">
-        <a>
-          <FontAwesomeIcon className="icon" icon={faPaperPlane} />
+        <a href="-" className="con-mail">
+          <FontAwesomeIcon className="icon mail" icon={faEnvelope} />
+          <span className="email-text">qwerty@gmail.com</span>
         </a>
-        <a>
-          <FontAwesomeIcon className="icon" icon={faEnvelope} />
+        <a href="-" className="con-t-w">
+          <FontAwesomeIcon className="icon t" icon={faTelegram} />
+        </a>
+        <a href="-" className="con-t-w">
+          <FontAwesomeIcon className="icon w" icon={faWhatsapp} />
         </a>
       </div>
     </header>
