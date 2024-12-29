4fd1b1f16311a87bd7a3f0bed72854b587e42597
IIIWinterIII <nikitavakulin39@gmail.com>
2024-12-29 14:25:37 +0300

    added logo created from text in p and faCode icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,20 +5,30 @@ import { Link, useLocation } from "react-router-dom";
 import routes from "../routes";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faCode, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faWhatsapp, faTelegram } from "@fortawesome/free-brands-svg-icons";
 
 function Header() {
   const { pathname } = useLocation();
-  
+
   return (
     <header>
-      <h1>Logo</h1>
+      <Link to="/" className="link-on-logo">
+        <div className="logo">
+          <FontAwesomeIcon icon={faCode} className="icon-logo" />
+          <p className="text-logo">N.K.</p>
+        </div>
+      </Link>
+
       <nav>
         <ul>
           {routes.map(({ path, name, component: Component }, index) => (
             <li key={index}>
-              <Link to={path} component={<Component />} className={`link ${path === pathname ? "active" : ''}`}>
+              <Link
+                to={path}
+                component={<Component />}
+                className={`link ${path === pathname ? "active" : ""}`}
+              >
                 <span>{name}</span>
               </Link>
             </li>
