74f270db5f6ad2471a6e765a0549be20b5ccb7fe
IIIWinterIII <nikitavakulin39@gmail.com>
2024-12-31 15:27:42 +0300

    finished style for fixed contact button

--- src/components/FixedButtonContact.jsx
+++ src/components/FixedButtonContact.jsx
@@ -6,7 +6,9 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 function FixedButtonContact() {
   return (
     <a className="fixed-contact-button" href="-">
-      <FontAwesomeIcon icon={faEnvelope} className="icon-fixed-button" />
+      <div className="icon-container">
+        <FontAwesomeIcon icon={faEnvelope} className="icon-fixed-button" />
+      </div>
     </a>
   );
 }
