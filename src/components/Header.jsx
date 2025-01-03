48a20dbb989df0b342c5d49d0d76da863d68c990
IIIWinterIII <nikitavakulin39@gmail.com>
2025-01-03 14:39:45 +0300

    adaptive for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,21 @@
 import "../styles/components/Header.scss";
- 
-import React from "react";
+
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import routes from "../routes";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCode, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faWhatsapp, faTelegram } from "@fortawesome/free-brands-svg-icons";
+import { faAngleDoubleRight } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   const { pathname } = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <header>
@@ -20,13 +26,14 @@ function Header() {
         </div>
       </Link>
 
-      <nav>
+      <nav className={`navigation ${isOpen ? "open" : ""}`}>
         <ul>
           {routes.map(({ path, name, component: Component }, index) => (
             <li key={index}>
               <Link
                 to={path}
                 component={<Component />}
+                onClick={toggleMenu}
                 className={`link ${path === pathname ? "active" : ""}`}
               >
                 <span>{name}</span>
@@ -47,6 +54,12 @@ function Header() {
           <FontAwesomeIcon className="icon w" icon={faWhatsapp} />
         </a>
       </div>
+      <button className="menu-toggle" onClick={toggleMenu}>
+        <FontAwesomeIcon
+          icon={faAngleDoubleRight}
+          className={`arrow ${isOpen ? "rotate" : ""}`}
+        />
+      </button>
     </header>
   );
 }
