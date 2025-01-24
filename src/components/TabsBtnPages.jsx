c12bad4fc4bd699dd787d7fdbe86943bf93f43c5
IIIWinterIII <nikitavakulin39@gmail.com>
2025-01-24 19:47:20 +0300

    creating a single database for templates and layouts

--- src/components/TabsBtnPages.jsx
+++ src/components/TabsBtnPages.jsx
@@ -3,57 +3,71 @@ import "../styles/components/TabsBtnPages.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
+import websiteTemplatesLP from "../websiteTemplatesLP.js";
+
 function TabsBtnPages() {
+  const templates = websiteTemplatesLP.filter(
+    (item) => item["title-keis-preview"] === "Шаблоны"
+  );
+
+  const mockups = websiteTemplatesLP.filter(
+    (item) => item["title-keis-preview"] === "Макеты"
+  );
+
   return (
     <div className="keis-preview">
       <h1 className="title-keis-preview">Шаблоны</h1>
 
       <div className="block-with-website-templates">
-        <div className="box-with-examples">
-          <div className="img-wrapper">
-            <img
-              className="img-preview"
-              src={`${process.env.PUBLIC_URL}/assets/images/construction.png`}
-              alt="construction"
-            />
-            <div className="description-and-button">
-              <p className="subtitle-website">
-                Одностраничный шаблон для строительной компании с современным
-                дизайном, содержащий информацию об услугах, портфолио, отзывы
-                клиентов и форму обратной связи.
-              </p>
-              <a
-                className="button-more-details"
-                href="https://iiiwinteriii.github.io/Construction/"
-              >
-                <span className="text">Посмотреть</span>
-                <FontAwesomeIcon className="arrow-right" icon={faArrowRight} />
-              </a>
+        {templates.map((item, index) => (
+          <div className="box-with-examples" key={index}>
+            <div className="img-wrapper">
+              <img
+                className="img-preview"
+                src={`${process.env.PUBLIC_URL}${item["img-preview"]}`}
+                alt={item.alt}
+              />
+              <div className="description-and-button">
+                <h2 className="title-website">{item["title-website"]}</h2>
+                <p className="subtitle-website">{item["subtitle-website"]}</p>
+                <a className="button-more-details" href={item.href}>
+                  <span className="text">Посмотреть</span>
+                  <FontAwesomeIcon
+                    className="arrow-right"
+                    icon={faArrowRight}
+                  />
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
+      </div>
 
-        <div className="box-with-examples">
-          <div className="img-wrapper">
-            <img
-              className="img-preview"
-              src={`${process.env.PUBLIC_URL}/assets/images/coffee.png`}
-              alt="coffee"
-            />
-            <div className="description-and-button">
-              <p className="subtitle-website">
-              Одностраничный шаблон для кофейни с уютным дизайном, включающий меню, акции, отзывы посетителей и форму для бронирования.
-              </p>
-              <a
-                className="button-more-details"
-                href="https://iiiwinteriii.github.io/Coffee/"
-              >
-                <span className="text">Посмотреть</span>
-                <FontAwesomeIcon className="arrow-right" icon={faArrowRight} />
-              </a>
+      <h1 className="title-keis-preview">Макеты</h1>
+
+      <div className="block-with-website-templates">
+        {mockups.map((item, index) => (
+          <div className="box-with-examples" key={index}>
+            <div className="img-wrapper">
+              <img
+                className="img-preview"
+                src={`${process.env.PUBLIC_URL}${item["img-preview"]}`}
+                alt={item.alt}
+              />
+              <div className="description-and-button">
+              <h2 className="title-website">{item["title-website"]}</h2>
+                <p className="subtitle-website">{item["subtitle-website"]}</p>
+                <a className="button-more-details" href={item.href}>
+                  <span className="text">Посмотреть</span>
+                  <FontAwesomeIcon
+                    className="arrow-right"
+                    icon={faArrowRight}
+                  />
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
--- src/websiteTemplatesLP.js
+++ src/websiteTemplatesLP.js
@@ -1,1 +1,51 @@
-const
+const websiteTemplatesLP = [
+    // ШАБЛОНЫ
+  {
+    "title-keis-preview": "Шаблоны",
+    "img-preview": "/assets/images/construction.png",
+    alt: "Construction",
+    "title-website": "Construction",
+    "subtitle-website":
+      "Одностраничный шаблон для строительной компании с современным дизайном, содержащий информацию об услугах, портфолио, отзывы клиентов и форму обратной связи.",
+    href: "https://iiiwinteriii.github.io/Construction/",
+  },
+  {
+    "title-keis-preview": "Шаблоны",
+    "img-preview": "/assets/images/coffee.png",
+    alt: "Coffee",
+    "title-website": "Coffee",
+    "subtitle-website":
+      "Одностраничный шаблон для кофейни с уютным дизайном, включающий меню, акции, отзывы посетителей и форму для бронирования.",
+    href: "https://iiiwinteriii.github.io/Coffee/",
+  },
+//   МАКЕТЫ
+  {
+    "title-keis-preview": "Макеты",
+    "img-preview": "/assets/images/constructionOfHouses.png",
+    alt: "Construction of houses",
+    "title-website": "Construction Of Houses",
+    "subtitle-website":
+      "Одностраничный шаблон для кофейни с уютным дизайном, включающий меню, акции, отзывы посетителей и форму для бронирования.",
+    href: "https://www.figma.com/proto/zsr7fYDu7XLy1AZMe9wKtF/%D0%9C%D0%BD%D0%BE%D0%B3%D0%BE%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%87%D0%BD%D1%8B%D0%B9-%D1%81%D0%B0%D0%B9%D1%82---%D0%A1%D1%82%D1%80%D0%BE%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D1%81%D1%82%D0%B2%D0%BE-%D0%B4%D0%BE%D0%BC%D0%BE%D0%B2-(Copy)?node-id=4240-12075&t=QLqavM0sImm0ODUU-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=4240%3A17796",
+  },
+  {
+    "title-keis-preview": "Макеты",
+    "img-preview": "/assets/images/onlineToolStore.png",
+    alt: "Online tool store",
+    "title-website": "Online Tool Store",
+    "subtitle-website":
+      "Одностраничный шаблон для кофейни с уютным дизайном, включающий меню, акции, отзывы посетителей и форму для бронирования.",
+    href: "https://www.figma.com/proto/jJVIZlC92ybb8BAgVOL0sO/%D0%98%D0%BD%D1%82%D0%B5%D1%80%D0%BD%D0%B5%D1%82-%D0%BC%D0%B0%D0%B3%D0%B0%D0%B7%D0%B8%D0%BD---%D0%94%D0%BE%D1%81%D1%82%D0%B0%D0%B2%D0%BA%D0%B0-%D1%81%D1%82%D1%80%D0%BE%D0%B9%D0%BC%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%B0%D0%BB%D0%BE%D0%B2-(Copy)?node-id=1-819&p=f&t=OiGWxmzfpCV7Yblp-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1",
+  },
+  {
+    "title-keis-preview": "Макеты",
+    "img-preview": "/assets/images/Loft-furniture.png",
+    alt: "Loft furniture",
+    "title-website": "Loft Furniture",
+    "subtitle-website":
+      "Одностраничный шаблон для кофейни с уютным дизайном, включающий меню, акции, отзывы посетителей и форму для бронирования.",
+    href: "https://www.figma.com/proto/XNDbPdZRNgdfO84gU8dZIO/%D0%BB%D0%BE%D1%84%D1%82-%D0%BC%D0%B5%D0%B1%D0%B5%D0%BB%D1%8C-(Copy)?node-id=6-17&p=f&t=zc3GMmctEt6OoJDl-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=6%3A17",
+  },
+];
+
+export default websiteTemplatesLP;
