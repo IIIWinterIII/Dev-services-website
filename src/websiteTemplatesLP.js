3cafa1f2dc37ae74784bb91956767fbbeaec1549
IIIWinterIII <nikitavakulin39@gmail.com>
2025-02-09 18:19:52 +0300

    replacement png for webp in landing pages info

--- src/websiteTemplatesLP.js
+++ src/websiteTemplatesLP.js
@@ -1,12 +1,12 @@
-const websiteTemplatesLP = [
+const webpsiteTemplatesLP = [
   // Tilda
   {
     "title-keis-preview": "Tilda",
     type: "landing-page" || "bussines-card",
-    "img-preview": "/assets/images/Coffee-Tilda.png",
+    "img-preview": "/assets/images/Coffee-Tilda.webp",
     alt: "Coffee Tilda",
-    "title-website": "Кофейня на Tilda",
-    "subtitle-website":
+    "title-webpsite": "Кофейня на Tilda",
+    "subtitle-webpsite":
       "Современный сайт для кофейни с меню, командой и предстоящими мероприятиями",
     href: "https://project8198265.tilda.ws/",
   },
@@ -14,30 +14,30 @@ const websiteTemplatesLP = [
   {
     "title-keis-preview": "Шаблоны",
     type: "landing-page" || "bussines-card",
-    "img-preview": "/assets/images/bakery.png",
+    "img-preview": "/assets/images/bakery.webp",
     alt: "Bakery",
-    "title-website": "Пекарня",
-    "subtitle-website":
+    "title-webpsite": "Пекарня",
+    "subtitle-webpsite":
       "Одностраничный шаблон для пекарни с уютным дизайном, содержащий меню, историю, отзывы клиентов и форму заказа.",
     href: "https://iiiwinteriii.github.io/bakery/",
   },
   {
     "title-keis-preview": "Шаблоны",
     type: "landing-page" || "bussines-card",
-    "img-preview": "/assets/images/construction.png",
+    "img-preview": "/assets/images/construction.webp",
     alt: "Construction",
-    "title-website": "Строительство",
-    "subtitle-website":
+    "title-webpsite": "Строительство",
+    "subtitle-webpsite":
       "Одностраничный шаблон для строительной компании с современным дизайном, содержащий информацию об услугах, портфолио, отзывы клиентов и форму обратной связи.",
     href: "https://iiiwinteriii.github.io/Construction/",
   },
   {
     "title-keis-preview": "Шаблоны",
     type: "landing-page" || "bussines-card",
-    "img-preview": "/assets/images/coffee.png",
+    "img-preview": "/assets/images/coffee.webp",
     alt: "Coffee",
-    "title-website": "Кофе",
-    "subtitle-website":
+    "title-webpsite": "Кофе",
+    "subtitle-webpsite":
       "Одностраничный шаблон для кофейни с уютным дизайном, включающий меню, акции, отзывы посетителей и форму для бронирования.",
     href: "https://iiiwinteriii.github.io/Coffee/",
   },
@@ -45,30 +45,30 @@ const websiteTemplatesLP = [
   {
     "title-keis-preview": "Макеты",
     type: "landing-page" || "bussines-card",
-    "img-preview": "/assets/images/constructionOfHouses.png",
+    "img-preview": "/assets/images/constructionOfHouses.webp",
     alt: "Construction of houses",
-    "title-website": "Строительство домов",
-    "subtitle-website":
+    "title-webpsite": "Строительство домов",
+    "subtitle-webpsite":
       "Информативный лендинг для компании по строительству домов, включающий подробности услуг, фото проектов, отзывы клиентов и форму для запроса консультации.",
     href: "https://www.figma.com/proto/zsr7fYDu7XLy1AZMe9wKtF/%D0%9C%D0%BD%D0%BE%D0%B3%D0%BE%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%87%D0%BD%D1%8B%D0%B9-%D1%81%D0%B0%D0%B9%D1%82---%D0%A1%D1%82%D1%80%D0%BE%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D1%81%D1%82%D0%B2%D0%BE-%D0%B4%D0%BE%D0%BC%D0%BE%D0%B2-(Copy)?node-id=4240-12075&t=QLqavM0sImm0ODUU-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=4240%3A17796",
   },
   {
     "title-keis-preview": "Макеты",
     type: "landing-page" || "bussines-card",
-    "img-preview": "/assets/images/onlineToolStore.png",
+    "img-preview": "/assets/images/onlineToolStore.webp",
     alt: "Online tool store",
-    "title-website": "Магазин инструментов",
-    "subtitle-website":
+    "title-webpsite": "Магазин инструментов",
+    "subtitle-webpsite":
       "Практичный сайт-визитка для магазина инструментов, строительных и не только, с каталогом товаров, преимуществами магазина и контактной информацией для покупателей.",
     href: "https://www.figma.com/proto/jJVIZlC92ybb8BAgVOL0sO/%D0%98%D0%BD%D1%82%D0%B5%D1%80%D0%BD%D0%B5%D1%82-%D0%BC%D0%B0%D0%B3%D0%B0%D0%B7%D0%B8%D0%BD---%D0%94%D0%BE%D1%81%D1%82%D0%B0%D0%B2%D0%BA%D0%B0-%D1%81%D1%82%D1%80%D0%BE%D0%B9%D0%BC%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%B0%D0%BB%D0%BE%D0%B2-(Copy)?node-id=1-819&p=f&t=OiGWxmzfpCV7Yblp-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1",
   },
   {
     "title-keis-preview": "Макеты",
     type: "landing-page" || "bussines-card",
-    "img-preview": "/assets/images/Loft-furniture.png",
+    "img-preview": "/assets/images/Loft-furniture.webp",
     alt: "Loft furniture",
-    "title-website": "Мебель в стиле лофт",
-    "subtitle-website":
+    "title-webpsite": "Мебель в стиле лофт",
+    "subtitle-webpsite":
       "Лаконичный сайт-визитка для компании, которая делает мебель в стиле лофт, с информацией о продукции, услугах по изготовлению на заказ и контактными данными.",
     href: "https://www.figma.com/proto/XNDbPdZRNgdfO84gU8dZIO/%D0%BB%D0%BE%D1%84%D1%82-%D0%BC%D0%B5%D0%B1%D0%B5%D0%BB%D1%8C-(Copy)?node-id=6-17&p=f&t=zc3GMmctEt6OoJDl-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=6%3A17",
   },
@@ -76,34 +76,34 @@ const websiteTemplatesLP = [
   {
     "title-keis-preview": "Макеты",
     type: "portfolio",
-    "img-preview": "/assets/images/Creator.png",
+    "img-preview": "/assets/images/Creator.webp",
     alt: "Creator",
-    "title-website": "Дизайнер",
-    "subtitle-website":
+    "title-webpsite": "Дизайнер",
+    "subtitle-webpsite":
       "Минималистичный сайт-портфолио для продуктового дизайнера из Берлина, фокусирующийся на удобных интерфейсах для стартапов и описания услуг.",
     href: "https://www.figma.com/proto/Z6bO7g2ECX5zScurymUc5i/Portfolio-Creator-(Copy)?node-id=1-445&p=f&t=B4kBkZejMxfcQjZg-0&scaling=scale-down-width&content-scaling=fixed&page-id=0%3A1",
   },
   {
     "title-keis-preview": "Макеты",
     type: "portfolio",
-    "img-preview": "/assets/images/John-Smith.png",
+    "img-preview": "/assets/images/John-Smith.webp",
     alt: "John Smith",
-    "title-website": "Джон Смит",
-    "subtitle-website":
+    "title-webpsite": "Джон Смит",
+    "subtitle-webpsite":
       "Современный сайт-портфолио Джона Смита, специализирующегося на создании красивых адаптивных веб-сайтов, включающий галерею работ и описание услуг.",
     href: "https://www.figma.com/proto/eAYkzXGe1ucoBoUifu2OBe/John-Smith-(Copy)?node-id=1-2&p=f&t=CaPygKhy4QZebwpe-0&scaling=scale-down-width&content-scaling=fixed&page-id=0%3A1",
   },
   {
     "title-keis-preview": "Макеты",
     type: "portfolio",
-    "img-preview": "/assets/images/Veronika.png",
+    "img-preview": "/assets/images/Veronika.webp",
     alt: "Veronika",
-    "title-website": "Вероника",
-    "subtitle-website":
+    "title-webpsite": "Вероника",
+    "subtitle-webpsite":
       "Стильный одностраничный сайт-портфолио, для эксперта по веб-дизайну и брендингу без программирования, с акцентом на визуальные проекты.",
     href: "https://www.figma.com/proto/1XujHTWs5QIkGYCjvXPcIu/Veronika-(Copy)?node-id=2-2&p=f&t=8WQv6gBFi34S9MWL-0&scaling=scale-down-width&content-scaling=fixed&page-id=0%3A1",
   },
   // Corporate
 ];
 
-export default websiteTemplatesLP;
+export default webpsiteTemplatesLP;
